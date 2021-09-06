4ba168f9095199f740f32afc3585e91b92e00391
chenshengen <enengege@gmail.com>
2021-09-06 20:18:45 +0800

    optimize grpc dial function: don’t lock it unless necessary

--- transport/grpc/dialer.go
+++ transport/grpc/dialer.go
@@ -16,21 +16,22 @@ import (
 	"sync"
 	"time"
 
+	"yeager/transport/grpc/pb"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/connectivity"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/keepalive"
-	"yeager/transport/grpc/pb"
 )
 
 type dialer struct {
-	tlsConfig *tls.Config
-	conn      *grpc.ClientConn
-	connMu    sync.RWMutex
+	tlsConf *tls.Config
+	conn    *grpc.ClientConn
+	connMu  sync.Mutex
 }
 
 func NewDialer(config *tls.Config) *dialer {
-	return &dialer{tlsConfig: config}
+	return &dialer{tlsConf: config}
 }
 
 func (d *dialer) DialContext(ctx context.Context, addr string) (net.Conn, error) {
@@ -52,32 +53,17 @@ func (d *dialer) DialContext(ctx context.Context, addr string) (net.Conn, error)
 	return streamToConn(stream, cancel), nil
 }
 
-// tryGetClientConn return true if get conn successfully
-func (d *dialer) tryGetClientConn() (*grpc.ClientConn, bool) {
-	d.connMu.RLock()
-	defer d.connMu.RUnlock()
-	if d.conn == nil || d.conn.GetState() == connectivity.Shutdown {
-		return nil, false
+func (d *dialer) grpcDial(addr string, ctx context.Context) (*grpc.ClientConn, error) {
+	// optimized
+	if d.conn != nil && d.conn.GetState() != connectivity.Shutdown {
+		return d.conn, nil
 	}
-	return d.conn, true
-}
 
-// trySetClientConn return true if set conn successfully
-func (d *dialer) trySetClientConn(conn *grpc.ClientConn) bool {
 	d.connMu.Lock()
 	defer d.connMu.Unlock()
 	if d.conn != nil && d.conn.GetState() != connectivity.Shutdown {
-		return false
-	}
-
-	d.conn = conn
-	return true
-}
-
-func (d *dialer) grpcDial(addr string, ctx context.Context) (*grpc.ClientConn, error) {
-	conn, ok := d.tryGetClientConn()
-	if ok {
-		return conn, nil
+		// meanwhile other goroutinue already dial new ClientConn
+		return d.conn, nil
 	}
 
 	opts := []grpc.DialOption{
@@ -86,25 +72,18 @@ func (d *dialer) grpcDial(addr string, ctx context.Context) (*grpc.ClientConn, e
 			Timeout: 30 * time.Second,
 		}),
 	}
-	if d.tlsConfig == nil {
+	if d.tlsConf == nil {
 		opts = append(opts, grpc.WithInsecure())
 	} else {
-		opts = append(opts, grpc.WithTransportCredentials(credentials.NewTLS(d.tlsConfig)))
+		opts = append(opts, grpc.WithTransportCredentials(credentials.NewTLS(d.tlsConf)))
 	}
-
 	conn, err := grpc.DialContext(ctx, addr, opts...)
 	if err != nil {
 		return nil, err
 	}
 
-	ok = d.trySetClientConn(conn)
-	if !ok {
-		// means that another goroutine has set the grpc connection,
-		// this unused connection shall be discard
-		conn.Close()
-	}
-
-	return d.conn, nil
+	d.conn = conn
+	return conn, nil
 }
 
 func (d *dialer) Close() error {
