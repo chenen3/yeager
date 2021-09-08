43d247d462985d80a0cec97fa69a8f0526756b0c
chenshengen <enengege@gmail.com>
2021-09-08 19:30:05 +0800

    enable grpc server keepalive

--- transport/grpc/dialer.go
+++ transport/grpc/dialer.go
@@ -62,16 +62,11 @@ func (d *dialer) grpcDial(addr string, ctx context.Context) (*grpc.ClientConn, e
 	d.connMu.Lock()
 	defer d.connMu.Unlock()
 	if d.conn != nil && d.conn.GetState() != connectivity.Shutdown {
-		// meanwhile other goroutinue already dial new ClientConn
+		// meanwhile other goroutine already dial new ClientConn
 		return d.conn, nil
 	}
 
-	opts := []grpc.DialOption{
-		grpc.WithKeepaliveParams(keepalive.ClientParameters{
-			Time:    60 * time.Second,
-			Timeout: 30 * time.Second,
-		}),
-	}
+	var opts []grpc.DialOption
 	if d.tlsConf == nil {
 		opts = append(opts, grpc.WithInsecure())
 	} else {
