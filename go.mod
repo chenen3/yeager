4d1bac0d2bcdee5915593b656272b6c42b3b90f4
chenshengen <enengege@gmail.com>
2021-09-10 15:11:50 +0800

    support QUIC transport

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/google/uuid v1.3.0
+	github.com/lucas-clemente/quic-go v0.23.0
 	github.com/prometheus/client_golang v1.11.0
 	github.com/v2fly/v2ray-core/v4 v4.42.1
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
@@ -15,14 +16,25 @@ require (
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/cheekybits/genny v1.0.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/marten-seemann/qtls-go1-16 v0.1.4 // indirect
+	github.com/marten-seemann/qtls-go1-17 v0.1.0 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/nxadm/tail v1.4.8 // indirect
+	github.com/onsi/ginkgo v1.16.4 // indirect
 	github.com/pires/go-proxyproto v0.6.0 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.26.0 // indirect
 	github.com/prometheus/procfs v0.6.0 // indirect
 	go.starlark.net v0.0.0-20210602144842-1cdb82c9e17a // indirect
+	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/sys v0.0.0-20210820121016-41cdb8703e55 // indirect
 	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/tools v0.1.1 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 )
---
+++ transport/quic/listener.go
@@ -0,0 +1,94 @@
+package quic
+
+import (
+	"context"
+	"crypto/tls"
+	"net"
+
+	quic "github.com/lucas-clemente/quic-go"
+	"yeager/log"
+)
+
+type Listener struct {
+	ctx       context.Context
+	cancelCtx context.CancelFunc
+	lis       quic.Listener
+	conns     chan net.Conn
+}
+
+func (l *Listener) accept() {
+	for {
+		sess, err := l.lis.Accept(l.ctx)
+		if err != nil {
+			select {
+			case <-l.ctx.Done():
+				return
+			default:
+				log.Error(err)
+				continue
+			}
+		}
+
+		go l.acceptStream(sess)
+	}
+}
+
+func (l *Listener) acceptStream(session quic.Session) {
+	defer session.CloseWithError(quic.ApplicationErrorCode(quic.NoError), "session closed")
+	for {
+		stream, err := session.AcceptStream(l.ctx)
+		if err != nil {
+			select {
+			case <-l.ctx.Done():
+				return
+			case <-session.Context().Done():
+				return
+			default:
+				log.Warn(err)
+				continue
+			}
+		}
+
+		select {
+		case <-stream.Context().Done():
+			continue
+		default:
+		}
+
+		l.conns <- &conn{
+			Stream:     stream,
+			localAddr:  session.LocalAddr(),
+			remoteAddr: session.RemoteAddr(),
+		}
+	}
+}
+
+func (l *Listener) Accept() (net.Conn, error) {
+	return <-l.conns, nil
+}
+
+func (l *Listener) Close() error {
+	l.cancelCtx()
+	return l.lis.Close()
+}
+
+func (l *Listener) Addr() net.Addr {
+	return l.lis.Addr()
+}
+
+func Listen(addr string, tlsConf *tls.Config) (net.Listener, error) {
+	lis, err := quic.ListenAddr(addr, tlsConf, &quic.Config{KeepAlive: true})
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	l := &Listener{
+		ctx:       ctx,
+		cancelCtx: cancel,
+		lis:       lis,
+		conns:     make(chan net.Conn, 32),
+	}
+	go l.accept()
+	return l, nil
+}
